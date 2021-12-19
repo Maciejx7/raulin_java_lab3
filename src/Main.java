07304c359f662dac58920f0ad37890f0237bd362
Maciejx7 <maciej.raulin@gmail.com>
2021-12-19 10:52:27 +0100

    Zadanie 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    /* zad.1
+        a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
+        b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
+        c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
+        d) niech w pętli są podawane wartości liczbowe z klawiatury,
+        e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
+        f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
+        do podanych cyfr przez użytkownika,
+        g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
+        */
+
+    }
+}
