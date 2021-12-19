21409d4f51dba0664ea8115c1132bab34023a220
Maciejx7 <maciej.raulin@gmail.com>
2021-12-19 11:11:28 +0100

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     /* zad.1
@@ -10,6 +11,56 @@ public class Main {
         do podanych cyfr przez użytkownika,
         g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
         */
+        enum LiczbyEnum {
+            Raz, Dwa, Trzy, Cztery, Pięć, Sześć;
+        }
+        enum StatusEnum {
+            KONIEC, KONTYNUUJ;
+        }
+        Scanner in = new Scanner(System.in);
+        int i;
+        while (true) {
+
+            i = in.nextInt();
+
+            if (i == 0) {
+
+                System.out.println(StatusEnum.KONIEC);
+
+                break;
+            }
+
+            switch (i) {
+
+                case 1:
+                    System.out.println(LiczbyEnum.Raz);
+                    break;
+
+                case 2:
+                    System.out.println(LiczbyEnum.Dwa);
+                    break;
+
+                case 3:
+                    System.out.println(LiczbyEnum.Trzy);
+                    break;
+                case 4:
+
+                    System.out.println(LiczbyEnum.Cztery);
+                    break;
+
+                case 5:
+                    System.out.println(LiczbyEnum.Pięć);
+                    break;
+
+                case 6:
+                    System.out.println(LiczbyEnum.Sześć);
+                    break;
+
+                default:
+                    System.out.println(StatusEnum.KONTYNUUJ);
+            }
+
+        }
 
     }
 }
